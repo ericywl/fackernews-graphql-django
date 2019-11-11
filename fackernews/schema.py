c23bdf8ccb64a5950773a0f7592f6b2d44a86c2e
ericywl <ericywl@live.com>
2019-11-11 18:08:03 +0800

    Add project

---
+++ fackernews/schema.py
@@ -0,0 +1,20 @@
+import graphene
+import graphql_jwt
+
+import links.schema
+import links.schema_relay
+import users.schema
+
+
+class Query(links.schema.Query, links.schema_relay.RelayQuery,
+            users.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
