79d43ea6b5b1086e461c7fa904fb724233431ecd
ericywl <ericywl@live.com>
2019-11-11 18:22:38 +0800

    Add relay mutation to create links

--- fackernews/schema.py
+++ fackernews/schema.py
@@ -11,7 +11,8 @@ class Query(links.schema.Query, links.schema_relay.RelayQuery,
     pass
 
 
-class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, links.schema_relay.RelayMutation,
+               users.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
--- links/schema_relay.py
+++ links/schema_relay.py
@@ -6,7 +6,7 @@ from graphene_django.filter import DjangoFilterConnectionField
 from .models import Link, Vote
 
 
-class LinkFilter(django_filers.FilterSet):
+class LinkFilter(django_filters.FilterSet):
     class Meta:
         model = Link
         fields = ['url', 'description']
@@ -16,7 +16,7 @@ class LinkNode(DjangoObjectType):
     class Meta:
         model = Link
         interfaces = (graphene.relay.Node, )
-        
+
 
 class VoteNode(DjangoObjectType):
     class Meta:
@@ -26,4 +26,29 @@ class VoteNode(DjangoObjectType):
 
 class RelayQuery(graphene.ObjectType):
     relay_link = graphene.relay.Node.Field(LinkNode)
-    relay_links = DjangoFilterConnectionField(LinkNode, filterset_class=LinkFilter)
+    relay_links = DjangoFilterConnectionField(
+        LinkNode, filterset_class=LinkFilter)
+
+
+class RelayCreateLink(graphene.relay.ClientIDMutation):
+    link = graphene.Field(LinkNode)
+
+    class Input:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate_and_get_payload(self, info, **input):
+        user = info.context.user or None
+
+        link = Link(
+            url=input.get('url'),
+            description=input.get('description'),
+            posted_by=user,
+        )
+        link.save()
+        
+        return RelayCreateLink(link=link)
+        
+        
+class RelayMutation(graphene.AbstractType):
+    relay_create_link = RelayCreateLink.Field()
